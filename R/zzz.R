bc4f780e8fe468ea864d20ed889663ba314a3a04
Yasuhiro Kojima <tiisaishima@gmail.com>
2019-03-25 23:01:26 -0400

    eliminate delayed assign

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ safe_import_from_path <- function(module.name, libname, pckgname){
     error = function(e){
       packageStartupMessage("Install python dependencies...")
       reticulate::py_install(c("numpy", "numba", "scipy", "progressbar2", "pandas"))
-      delayedAssign(module.name, reticulate::import_from_path(module.name, path = file.path(libname, pckgname, "src")), assign.env = .GlobalEnv)
+      assign(module.name, reticulate::import_from_path(module.name, path = file.path(libname, pckgname, "src")), assign.env = .GlobalEnv)
       
     })
 }
